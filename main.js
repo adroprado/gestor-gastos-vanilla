1f87dde18bc6881fdc656ed179aa40d3b1a39738
Alejandro Prado <adro.viprado@gmail.com>
2026-01-13 07:43:25 -0600

    feat(main) agregar variables iniciales
    Se agregan variables que almacenan elementos del DOM

---
+++ main.js
@@ -0,0 +1,7 @@
+const $form = document.querySelector(".form"),
+  $table = document.querySelector(".table"),
+  $total = document.querySelector(".total-amount"),
+  $template = document.querySelector(".template").content,
+  $fragment = document.createDocumentFragment();
+
+console.log($form, $table, $total, $template, $fragment);
