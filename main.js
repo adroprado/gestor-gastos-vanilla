51320c1ad73bc76cc9e02fe0d4668dc65ae6d410
Alejandro Prado <adro.viprado@gmail.com>
2026-01-15 16:14:22 -0600

    feat(leer): agregar función para leer los gastos de db
    Se realiza la acción leer(read) del CRUD

--- main.js
+++ main.js
@@ -22,24 +22,39 @@ const crearGasto = () => {
   baseDeDatosInicial = nuevoGasto;
 
   $form.reset();
-  console.log(baseDeDatosInicial);
 };
 
 // Arrow function: manejo de persistencia de datos en localStorage
-const gastosSetItemLS = () =>
+const gastosSet = () =>
   ls.setItem("gastos", JSON.stringify(baseDeDatosInicial));
 
 // Arrow function: extracción de datos del localStorage
-const gastosGetItemLS = () => JSON.parse(ls.getItem("gastos"));
+const gastosGet = () => {
+  let datosDeLS = JSON.parse(ls.getItem("gastos"));
+  baseDeDatosInicial = datosDeLS || [];
+};
 
-gastosGetItemLS();
+gastosGet();
 
+// Arrow function: lectura de DB y agregando elementos al DOM
+const leerGastos = () => {
+  baseDeDatosInicial.forEach((el) => {
+    let $clon = d.importNode($plantilla, true);
+    $clon.querySelector(".nombre").textContent = el.nombre;
+    $clon.querySelector(".monto").textContent = el.cantidad;
+
+    $fragmento.appendChild($clon);
+  });
+
+  $tabla.querySelector("tbody").appendChild($fragmento);
+};
+
+d.addEventListener("DOMContentLoaded", leerGastos);
 // Manejo de delegación de eventos
 d.addEventListener("submit", (e) => {
   if (e.target === $form) {
     e.preventDefault();
-
     crearGasto();
-    gastosSetItemLS();
+    gastosSet();
   }
 });
